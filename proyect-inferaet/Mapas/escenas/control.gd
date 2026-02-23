266e0962f17084827288be3080da11db4bb633c1
robersider <robersider@gmail.com>
2026-02-23 21:01:08 +0100

    Revert "Sistema de exp sin mapa, revisar si funciona"
    This reverts commit 70512debfc151ed555343eb86d0fc99cdd749466, reversing
    changes made to 8cf957f2627fed408b073e2ef52c487f5a310de8.

--- proyect-inferaet/Mapas/escenas/control.gd
+++ proyect-inferaet/Mapas/escenas/control.gd
@@ -3,16 +3,15 @@ extends Control
 @onready var timer: Timer = $Timer
 @onready var label: Label = $Label
 
-var total_seconds: int = 300  # 5 minutos = 300 segundos
+var total_seconds: int = 0
 
 func _ready():
 	custom_minimum_size = Vector2(200, 60)
 	label.set_anchors_preset(Control.PRESET_FULL_RECT)
 	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	label.text = "00:00"
 	label.add_theme_color_override("font_color", Color.ORANGE_RED)
-	
-	_update_label()
 	
 	timer.wait_time = 1.0
 	timer.one_shot = false
@@ -24,16 +23,11 @@ func _ready():
 
 
 func _on_timer_timeout():
-	if total_seconds > 0:
-		total_seconds -= 1
-		_update_label()
-	else:
-		timer.stop()  # Se detiene al llegar a 00:00
-
-
-func _update_label():
+	total_seconds += 1
+	
 	var minutes: int = total_seconds / 60
 	var seconds: int = total_seconds % 60
+	
 	label.text = "%02d:%02d" % [minutes, seconds]
 
 
@@ -41,6 +35,6 @@ func _update_position():
 	var mode = DisplayServer.window_get_mode()
 	
 	if mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
-		offset_top = 15
+		offset_top = 15   
 	else:
-		offset_top = 20
+		offset_top = 20   
